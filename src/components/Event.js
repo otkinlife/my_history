1561d292e383efa5ceb997c8b7c929e1d8211c7a
kcjia <otkinlife@163.com>
2024-03-17 14:32:39 +0800

    v1.0.0

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Image, ListGroup } from 'react-bootstrap';
+import Markdown from 'react-markdown';
 
-function Event({ event, figures }) {
+function Event({ figureName, figures }) {
   const [show, setShow] = React.useState(false);
   const [selectedFigure, setSelectedFigure] = React.useState(null);
 
-  const handleShow = (figure) => {
+  const handleShow = () => {
+    const figure = figures[figureName];
     setSelectedFigure(figure);
     setShow(true);
   };
@@ -15,17 +17,45 @@ function Event({ event, figures }) {
   };
 
   return (
-    <>
-      {event.related_figures.map((figureName, index) => (
-        <Button key={index} onClick={() => handleShow(figures[figureName])}>{figureName}</Button>
-      ))}
+    <div>
+      <Button 
+        size="sm" 
+        style={{ 
+          marginLeft:'0.5rem', 
+          fontSize: '0.75rem', 
+          backgroundColor: 'white', 
+          color: '#007FFF', 
+          border: '1px solid #007FFF' 
+        }} 
+        onClick={handleShow}
+      >
+        {figureName}
+      </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{selectedFigure?.name}</Modal.Title>
+          <Modal.Title>{selectedFigure ? selectedFigure.name : "未知"}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{selectedFigure?.desc}</Modal.Body>
+        <Modal.Body style={{ margin: '20px' }}>
+          <div style={{ display: 'flex', marginBottom: '15px' }}>
+            <Image 
+              src={selectedFigure && selectedFigure.avatar ? selectedFigure.avatar : process.env.PUBLIC_URL+"/static/default_avatar.png"} 
+              rounded 
+              style={{ width: '100px', height: '100px' }}
+            />
+            <div style={{ marginLeft: '15px' }}>
+              <Markdown>{`**\`生年：\`** ${selectedFigure ? selectedFigure.live_date : "未知"}`}</Markdown>
+              <Markdown>{`**\`别名：\`** ${selectedFigure && selectedFigure.alias ? selectedFigure.alias.join(', ') : "无"}`}</Markdown>
+            </div>
+          </div>
+          {selectedFigure && selectedFigure.desc ? selectedFigure.desc.map((item, index) => 
+            <ListGroup.Item key={index}><Markdown>{item}</Markdown></ListGroup.Item>
+          ) : <p>没有配置此人物的信息</p>}
+          {selectedFigure && selectedFigure.detail_url && 
+            <a href={selectedFigure.detail_url} target="_blank" rel="noreferrer">查看更多信息</a>
+          }
+        </Modal.Body>
       </Modal>
-    </>
+    </div>
   );
 }
 
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Chrono } from 'react-chrono';
+import Markdown from 'react-markdown';
 import Event from './Event';
+
 
 function Timeline({ events, figures }) {
   // 转换数据格式以适应 react-chrono
   const items = events.sort((a, b) => a.year - b.year).map(event => ({
     title: event.year.toString(),
     cardTitle: event.title,
-    cardSubtitle: event.desc,
-    cardDetailedText: event.related_figures.join(", ")
-  }));
+    cardSubtitle: (
+      <div style={{ marginLeft: '0.7rem' }}>
+        <Markdown>
+          {`**相关人物：**`}
+        </Markdown>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {event.related_figures.map(figure =>
+            <Event key={figure} figureName={figure} figures={figures} />
+          )}
+        </div>
+      </div>
 
-  return <Chrono items={items} mode="VERTICAL_ALTERNATING" />;
+    ),
+    url: event.url,
+    media: {
+      type: "IMAGE",
+      source: {
+        url: event.img ? `${process.env.PUBLIC_URL}/static/${event.img}` : `${process.env.PUBLIC_URL}/static/default.png`
+      }
+    },
+    cardDetailedText: (
+      <Markdown>
+        {`**描述：**\n- ${event.desc.join('\n- ')}\n`}
+      </Markdown>
+    ),
+  }));
+  return <Chrono items={items} mode="VERTICAL_ALTERNATING" cardHeight={400} slideShow />;
 }
 
 export default Timeline;
