d68df1c86dfdee1107ccaf0ff2d30774f43458c0
kcjia <otkinlife@163.com>
2024-03-17 21:10:32 +0800

    v1.0.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Timeline from './components/Timeline';
-import events from './data/events.json'; 
-import figures from "./data/figures.json";
 
 function App() {
-  return <Timeline events={events} figures={figures} />;
+  return <Timeline />;
 }
 
 export default App;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chrono } from 'react-chrono';
+import { Form } from 'react-bootstrap';
 import Markdown from 'react-markdown';
 import Event from './Event';
+import epochsData from './epoch.json'; // 直接从 JSON 文件中导入数据
 
+function Timeline() {
+  const [epoch, setEpoch] = useState(null);
+  const [epochs, setEpochs] = useState({});
+  const [events, setEvents] = useState([]);
+  const [figures, setFigures] = useState({});
+  const [loading, setLoading] = useState(true); // 添加一个新的状态变量
 
-function Timeline({ events, figures }) {
-  // 转换数据格式以适应 react-chrono
+  // 设置 epochs 并选择第一个时期
+  useEffect(() => {
+    setEpochs(epochsData);
+    setEpoch(Object.keys(epochsData)[0]);
+  }, []);
+
+  // 当 epoch 改变时，重新加载事件和人物
+  useEffect(() => {
+    if (epoch) {
+      setLoading(true); // 开始加载数据
+      Promise.all([
+        fetch(process.env.PUBLIC_URL + "/data/" + epochs[epoch].events_path)
+          .then(response => response.json())
+          .then(data => setEvents(data)),
+        fetch(process.env.PUBLIC_URL + "/data/" + epochs[epoch].figures_path)
+          .then(response => response.json())
+          .then(data => setFigures(data))
+      ]).then(() => setLoading(false)); // 数据加载完毕
+    }
+  }, [epoch, epochs]);
+
   const items = events.sort((a, b) => a.year - b.year).map(event => ({
     title: event.year.toString(),
     cardTitle: event.title,
@@ -20,7 +47,6 @@ function Timeline({ events, figures }) {
           )}
         </div>
       </div>
-
     ),
     url: event.url,
     media: {
@@ -35,7 +61,16 @@ function Timeline({ events, figures }) {
       </Markdown>
     ),
   }));
-  return <Chrono items={items} mode="VERTICAL_ALTERNATING" cardHeight={400} slideShow />;
+
+  return (
+    <div>
+      <Form.Control as="select" custom value={epoch} onChange={e => setEpoch(e.target.value)}>
+        <option value="">选择一个时期</option>
+        {Object.keys(epochs).map(epoch => <option key={epoch}>{epoch}</option>)}
+      </Form.Control>
+      {!loading && epoch && <Chrono items={items} mode="VERTICAL_ALTERNATING" cardHeight={400} slideShow />}
+    </div>
+  );
 }
 
 export default Timeline;
